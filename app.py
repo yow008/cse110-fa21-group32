25b967ac78d8730181cc3f395309863e2654b70d
Hannah Hui <hannah.hui5184@gmail.com>
2021-11-22 15:02:24 -0800

    Integration - #67 Fixed update and request bugs

--- app.py
+++ app.py
@@ -21,23 +21,42 @@ def home_page():
     if request.method == 'POST':
         msg = request.get_json()
         if msg['type'] == 'register':
-            #user_db.createUser(msg['username'], msg['password'], msg['email'], '', '')
+            user_db.createUser(msg['username'], msg['password'], msg['email'], '', '')
+            return {'msg': 'Success!'}, 201
+        if msg['type'] == 'delete':
+            user_db.deleteUser(msg['username'], msg['password'])
             return {'msg': 'Success!'}, 201
 
     if request.method == 'GET':
         msg = request.args
-        print("HERE")
         if msg['type'] == 'search':
             keyword = msg['keyword']
             recipes = recipe_db.searchRecipeByKeyword(keyword)
             recipes = {i: recipes[i] for i in range(len(recipes))}
-            recipe_json = json.dumps(recipes, indent = 2) 
+            recipe_json = json.dumps(recipes, indent=2) 
             # print(recipe_json)
             response = Response(response=recipe_json, status=200, mimetype='application/json')
             print(response.response)
             return response
         elif msg['type'] == 'login':
-            pass
+            username = msg['user']
+            password = msg['pass']
+            userInfo = user_db.request(username, password, ['Username', 'Password'])
+            data = {"userInfo": userInfo}
+            data_json = json.dumps(data, indent=2)
+            response = Response(response=data_json, status=200, mimetype='application/json')
+            return response
+        elif msg['type'] == 'request':
+            username = msg['user']
+            password = msg['pass']
+            elem = msg['elem']
+            userInfo = user_db.request(username, password, ['Email'])
+            print(username, password, elem)
+            data = {"userInfo": userInfo}
+            print(userInfo)
+            data_json = json.dumps(data, indent=2)
+            response = Response(response=data_json, status=200, mimetype='application/json')
+            return response
         
     return {'msg':'test'}, 200
 
--- user_db.py
+++ user_db.py
@@ -55,22 +55,19 @@ class User_DB:
             print('ERROR: User ' + username + ' already exists.')
             return False
 
-    def login(self, username, password):
-        self.cur.execute('SELECT COUNT(*) FROM Users WHERE username = ? AND password = ?', (key, username, password))
-        return count != 0
-
-    def request(self, username, password, key):
+    def request(self, username, password, keys):
         '''
         TODO: Retrieve DB[key] 
         Inputs: 
         - username: string
         - password: string
+        - keys: list of keys to query
         Output:
         - requested info or None if username/passwd don't match/exist
         '''
-
-        self.cur.execute('SELECT ? FROM Users WHERE username = ? AND password = ?', (key, username, password))
+        self.cur.execute('SELECT %s FROM Users WHERE Username = ? AND Password = ?' % (', '.join(keys)), (username, password))
         profile = self.cur.fetchall() # fetchall returns all matched records, i.e. correct username & password
+        print(profile)
         if len(profile) > 1: # since no duplicate allowed, should have only one profile
             print('ERROR: Duplicate users found.')
             return None
@@ -79,6 +76,39 @@ class User_DB:
         else:
             return profile[0]
 
+    # def request2(self, username, password, key):
+    #     '''
+    #     TODO: Retrieve DB[key] 
+    #     Inputs: 
+    #     - username: string
+    #     - password: string
+    #     Output:
+    #     - requested info or None if username/passwd don't match/exist
+    #     '''
+    #     print(username, password)
+    #     self.cur.execute('SELECT * FROM Users WHERE username = ? AND password = ?', (username, password))
+    #     profile = self.cur.fetchall() # fetchall returns all matched records, i.e. correct username & password
+    #     if len(profile) > 1: # since no duplicate allowed, should have only one profile
+    #         print('ERROR: Duplicate users found.')
+    #         return None
+    #     elif len(profile) == 0: # user not found / passwd incorrect
+    #         return None
+    #     else:
+    #         return profile[0]
+
     # TODO: Finalize CRUD functions
+
+    def updateUser(self, username, password, updateVals):
+        #Check to see if the sought updated columns exist in the database at all
+        query = 'UPDATE Users SET ' + ', '.join(['%s = ?' % (p[0]) for p in updateVals.items()]) + ' WHERE username = ? AND password = ?'
+        self.cur.execute(query, (*[p[1] for p in updateVals.items()], username, password))
+        self.conn.commit()
+    
+    def deleteUser(self,username,password):
+        print("HERE")
+        # Updates users with username, password, email, first & last name. Other stuff will be NULL.
+        self.cur.execute("DELETE FROM Users WHERE username = ? AND password = ? ", (username, password))
+        self.conn.commit()
+        return True
 
 new_db = User_DB()
