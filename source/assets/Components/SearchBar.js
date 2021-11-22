7d88a3ecd0226e3f62c91088a193905be89e64b2
Hannah Hui <hannah.hui5184@gmail.com>
2021-11-22 06:02:04 -0800

    Integration - #67 search with results from backend
    Co-authored-by: StuartBoynton <StuartBoynton1@users.noreply.github.com>
    Co-authored-by: ouke025 <ouke025@users.noreply.github.com>
    Co-authored-by: Chenxing Qiu <c3qiu@users.noreply.github.com>

--- source/assets/Components/SearchBar.js
+++ source/assets/Components/SearchBar.js
@@ -2,42 +2,94 @@
 
 import { Router } from '../scripts/Router.js';
 
+// TODO: edit the local server URL
+const LOCAL_URL = 'http://127.0.0.1:5000';
 const router = new Router();
 
 class SearchBar extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
 
-        // Create styles and root element
-        const styles = document.createElement('style');
-        const article = document.createElement('article');
+    // Create styles and root element
+    const styles = document.createElement('style');
+    const article = document.createElement('article');
 
-        // Fill in styles and root element
-        styles.innerHTML = ``;
-        article.innerHTML = `
+    // Fill in styles and root element
+    styles.innerHTML = ``;
+    article.innerHTML = `
         <p>Search Bar</p>
-            <input type="text" placeholder="Search.." name="search">
-            <button id="search-icon">Search</button>
+        <form id="searchFormID" name="search-form">
+          <input type="text" placeholder="Search..." name="search-phrase">
+          <button type="submit" id="search-icon">Submit</button>
+        </form>
         `;
 
-        // Append elements to the shadow root
-        this.shadowRoot.append(styles, article);
+    // Append elements to the shadow root
+    this.shadowRoot.append(styles, article);
 
-        router.addPage('search-results', function() {
-            document.getElementById('#section--home').classList.remove('shown');
-            document.getElementById('#section--search-bar').classList.remove('shown');
+    // Add search results page to router in order to successfully navigate to it
+    router.addPage('search-results', function () {
+      document.getElementById('#section--home').classList.remove('shown');
+      document.getElementById('#section--search-bar').classList.remove('shown');
 
-            console.log(document.getElementById('#section--search-results'));
-            document.getElementById('#section--search-results').classList.add('shown');
-                
-        });
-        
-        const recipePage = this.shadowRoot.getElementById('search-icon');
-        recipePage.addEventListener('click', () => {
-            router.navigate('search-results');
-        });
+      document
+        .getElementById('#section--search-results')
+        .classList.add('shown');
+    });
+
+    // Search for recipes from Spoonacular upon form submit
+    const searchForm = this.shadowRoot.getElementById('searchFormID');
+    searchForm.addEventListener('submit', (e) => {
+      let formData = new FormData(searchForm);
+
+      // Clears any previous search results
+      document.getElementById('#section--search-results').innerHTML = '';
+      const searchResultsPage = document.createElement('search-results-page');
+      document
+        .getElementById('#section--search-results')
+        .appendChild(searchResultsPage);
+
+      // Searches for recipes, then navigates to populated search results
+      // page after search results are returned
+      searchRecipes(formData.get('search-phrase'), searchResultsPage);
+
+      // Necessary to prevent automatic submission
+      e.preventDefault();
+    });
+  }
+}
+
+/**
+ * Uses a search phrase to search through Spoonacular recipes. After
+ * recipes are found, populate the search results page and redirect there.
+ * @param {String} searchPhrase
+ * @param {SearchResultsPage} searchResPage
+ */
+function searchRecipes(searchPhrase, searchResPage) {
+  fetch(
+    // need to encode with UTF-8 for special characters like ' '
+    `${LOCAL_URL}?type=search&keyword=${encodeURIComponent(searchPhrase)}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
     }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // populates search results page and redirects there
+      searchResPage.results = data;
+      router.navigate('search-results');
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 }
 
 customElements.define('search-bar', SearchBar);
--- source/assets/Components/SearchResults.js
+++ source/assets/Components/SearchResults.js
@@ -1,29 +1,95 @@
 // SearchResults.js
+import { Router } from '../scripts/Router.js';
+const router = new Router();
 
 class SearchResultsPage extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
 
-        // Create styles and root element
-        const styles = document.createElement('style');
-        const article = document.createElement('article');
+    // Create styles and root element
+    const styles = document.createElement('style');
 
-        // Fill in styles and root element
-        styles.innerHTML = ``;
-        article.innerHTML = `
-        <h2>Search Results</h2>
-        <p>Contents</p>
-        <ul>
-          <li> <button type="menu">Recipe 1 (not linked)</button></li>
-          <li> <button type="menu">Recipe 2 (not linked)</button></li>
-          <li> <button type="menu">Recipe 3 (not linked)</button></li>
-        </ul>
-        `;
+    // Fill in styles and root element
+    styles.innerHTML = ``;
 
-        // Append elements to the shadow root
-        this.shadowRoot.append(styles, article);
-    }
+    // Append elements to the shadow root
+    this.shadowRoot.append(styles);
+  }
+
+  /**
+   * TODO:
+   * @param {Objects} results
+   */
+  set results(results) {
+    if (!results) return;
+
+    this.data = results;
+
+    const section = document.createElement('section');
+    section.setAttribute('id', 'section--recipe-res');
+
+    createRecipeCards(results, section);
+
+    this.shadowRoot.append(section);
+    console.log(this.shadowRoot);
+  }
 }
 
+/**
+ * TODO:
+ * @param {Objects} results
+ * @param {Object} section
+ */
+function createRecipeCards(results, section) {
+  // Go through every one of the result recipes
+  Object.keys(results).forEach(function (key) {
+    const data = results[key];
+
+    // Add recipe title
+    const title = document.createElement('p');
+    title.classList.add('title');
+    title.innerText = data.title;
+
+    // Add recipe picture
+    const image = document.createElement('img');
+    image.setAttribute('src', data.image);
+    image.setAttribute('alt', data.title);
+
+    // Add all the elements to the card
+    const card = document.createElement('div');
+    card.setAttribute('id', data.id);
+    card.appendChild(image);
+    card.appendChild(title);
+
+    // Add all elements inside a link
+    const link = document.createElement('a');
+    link.setAttribute('href', `home.html#recipe${data.id}`);
+
+    link.appendChild(card);
+    section.appendChild(link);
+
+    // Add the corresponding expand recipe view to router
+    addPage(data.id);
+  });
+}
+
+/**
+ * TODO:
+ * @param {String} recipeID
+ */
+function addPage(recipeID) {
+  router.addPage(`#recipe${recipeID}`, function () {
+    document.getElementById('#section--home').classList.remove('shown');
+    document.getElementById('#section--search-bar').classList.remove('shown');
+
+    document
+      .getElementById('#section--search-results')
+      .classList.remove('shown');
+
+    document.getElementById('#section--recipe').classList.add('shown');
+  });
+}
+
+// Define the Class so it can be used as a custom element
 customElements.define('search-results-page', SearchResultsPage);
