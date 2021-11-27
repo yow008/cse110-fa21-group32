81a42fc397780c24a150cc8b03e77edc4a039f3a
You Wu <89385803+yow008@users.noreply.github.com>
2021-11-27 15:54:22 -0800

    Added the Table structure for our grocery list.
    This code has not been integrated with backend, so the fetch() and .then have not been tested. The update is that I added my code for building a table containing "ingredient name + amount + unit" in each row.

--- source/assets/Components/Grocery.js
+++ source/assets/Components/Grocery.js
@@ -27,22 +27,77 @@ class GroceryPage extends HTMLElement {
           }
         `;
     article.innerHTML = `
-        <h2>Grocery List</h2>
-        <ul>
-            <input type="checkbox" checked>
-            <label>some item</label>
-            <br>
-            <input type="checkbox">
-            <label>some item</label>
-            <br>
-            <input type="checkbox" checked>
-            <label>some item</label>
-            <br>
-        </ul>
+    <h2>Grocery List</h2>
+    <div id="grocery-list">
+    </div>
+    <!--Add Recipe Ingredients-->
+    <p> Table </p>
+    <table>
+        <tr>
+        </tr>
+    </table>
+    <p> Unordered List </p>
+    <ul>
+        <input type="checkbox" checked>
+        <label>some item</label>
+        <br>
+        <input type="checkbox">
+        <label>some item</label>
+        <br>
+        <input type="checkbox" checked>
+        <label>some item</label>
+        <br>
+    </ul>
+    <button>Add (Add Fixed Item.) </button>
+    <button id="test">Test (For Integration Testing) </button>
         `;
 
     // Append elements to the shadow root
     this.shadowRoot.append(styles, article);
+    
+    /* ADDED */
+     /* Dummy Test. Delete this after integration with backend. */
+    var name = "Ingredient name";
+    var amount = 3;
+    var unit = "mg";
+    this.shadowRoot.querySelector("button").addEventListener("click", e => {
+        e.preventDefault();   
+        let ingredientsList = this.shadowRoot.querySelector('table');
+        ingredientsList.innerHTML += '<tr><td><input type="checkbox"></td><td>' + name + '</td><td>' + amount + unit + '</td></tr>'
+        //ingredientsList.innerHTML += ingredient;
+    });
+
+    /* Integration Test. Modify this. */
+    this.shadowRoot.getElementById("test").addEventListener("click", e => {
+        e.preventDefault();   
+        loadIngredientList()
+    });
+
+    function loadIngredientList() {
+    
+    /* NOT SURE About fetch() */
+    fetch('http://127.0.0.1:5000', {
+        method: 'GET', // or 'PUT'
+        headers: {
+          'Content-Type': 'img',
+        },
+        //body: JSON.stringify(data),
+        //body: imgList[0].files[0]
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          //   window.location.href = 'home.html';
+            console.log('Success:', data);
+            let fullIngredientListPos = this.shadowRoot.querySelector('table');
+            let fullIngredientList = data.recipe.extendedIngredients;
+            for(let i = 0; i < fullIngredientList.length; i++) {
+                fullIngredientListPos.innerHTML += '<tr><td><input type="checkbox"></td><td>' + extendedIngredients[i].name + '</td><td>' + extendedIngredients[i].amount + extendedIngredients[i].unit + '</td></tr>';
+            }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
   }
 }
 
