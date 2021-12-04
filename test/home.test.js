d3b8f83a84fb59b9156fb59dc17e37cd8034d809
Martin-Flores1023 <maf001@ucsd.edu>
2021-12-04 12:24:30 -0800

    Added some more tests

--- test/home.test.js
+++ test/home.test.js
@@ -1,3 +1,4 @@
+global.window = { location: { pathname: null } };;
 describe('Basic user flow for Website', () => {
     beforeAll(async () => {
       await page.goto(
@@ -16,35 +17,98 @@ describe('Basic user flow for Website', () => {
   
       expect(numberSidePanelButtons).toBe(1);
     });
-  });
-
-  describe("Test title of the homepage", () => {
-    beforeAll(async () => {
-        await page.goto(
-          'http://127.0.0.1:5500/source/home.html'
-        );
-      });
-    test("Title of the page", async () => {
-        const title = await page.title();
-        console.log(title);
-        expect(title).toBe("CookEZ");
-    }, 1000);
-});
-
-
-describe("Test navigation to Profile", () => {
-    beforeAll(async () => {
-        await page.goto(
-          'http://127.0.0.1:5500/source/home.html'
-        );
-      });
-    test("Testing nav to profile", async () => {
-            let item = await page.$('search-icon');
-            let itemShadow = await item.getProperty('shadowRoot');
-            let butt = await itemShadow.$('button');
-            await butt.click();
-            let inText = await butt.getProperty('innerText');
-            expect(inText['_remoteObject'].value).toBe("Search...");
-          }, 2500);
-});
+    test("Title of the Home page", async () => {
+      const title = await page.title();
+      console.log(title);
+      expect(title).toBe("CookEZ");
+  }, 1000);
+  test('should change page when button is clicked', async() => {
+    var button = await page.$('button#LinkToCalendar');
     
+    await button.click();
+    await page.waitForNavigation();
+    const result = await page.evaluate(() => {
+     return JSON.stringify(window.location.href);
+    });
+    console.log(result);
+    expect(result).toBe("\"http://127.0.0.1:5500/source/full_calendar/calendar.html\"");
+  });
+  // test('should change page when button is clicked', async() => {
+  //   var button = await page.$('button#LinkToProfile');
+  //   console.log(button);
+  //   await button.click();
+  //   await page.waitForNavigation();
+  //   const result = await page.evaluate(() => {
+  //     return JSON.stringify(window.location.href);
+  //    });
+  //    console.log(result);
+  //   expect(result).toBe("\"http://127.0.0.1:5500/source/home.html#profile");
+  // },10000);
+});
+
+//   describe("Test title of the homepage", () => {
+//     beforeAll(async () => {
+//         await page.goto(
+//           'http://127.0.0.1:5500/source/home.html'
+//         );
+//       });
+//     test("Title of the page", async () => {
+//         const title = await page.title();
+//         console.log(title);
+//         expect(title).toBe("CookEZ");
+//     }, 1000);
+// });
+
+// describe("Test navigation from home to profile", () => {
+//   beforeAll(async () => {
+//       await page.goto(
+//         'http://127.0.0.1:5500/source/home.html'
+//       );
+//     });
+//     test('should change page when button is clicked', async() => {
+//       var button = await page.$('button#LinkToProfile');
+      
+//       await button.click();
+//       await page.waitForNavigation();
+//       let result = await page.url()
+//       expect(result).toBe("\"http://127.0.0.1:5500/source/home.html#profile");
+//     },10000);
+// });
+
+// describe("Test Navigation from Home to Calendar", () => {
+//   beforeAll(async () => {
+//       await page.goto(
+//         'http://127.0.0.1:5500/source/home.html'
+//       );
+//     });
+//     test('should change page when button is clicked', async() => {
+//       var button = await page.$('button#LinkToCalendar');
+      
+//       await button.click();
+//       await page.waitForNavigation();
+//       const result = await page.evaluate(() => {
+//        return JSON.stringify(window.location.href);
+//       });
+//       console.log(result);
+//       expect(result).toBe("\"http://127.0.0.1:5500/source/full_calendar/calendar.html\"");
+//     });
+// });
+
+// describe("Test Navigation from SidePanel to Add Recipe", () => {
+//   beforeAll(async () => {
+//       await page.goto(
+//         'http://127.0.0.1:5500/source/home.html'
+//       );
+//     });
+//     test('should change page when button is clicked', async() => {
+//       var button = await page.$('a#LinkToAdd');
+      
+//       await button.click();
+//       await page.waitForNavigation();
+//       const result = await page.evaluate(() => {
+//        return JSON.stringify(window.location.href);
+//       });
+//       console.log(result);
+//       expect(result).toBe("\"http://127.0.0.1:5500/source/full_calendar/calendar.html\"");
+//     });
+// });
--- test/userLogin.test.js
+++ test/userLogin.test.js
@@ -11,3 +11,23 @@ describe("Test title of userLogin", () => {
     }, 1000);
 });
   
+const assert = require('assert');
+
+
+describe('Login Page', function () {
+    beforeAll(async () => {
+        await page.goto(
+          'http://127.0.0.1:5500/source/userLogin.html'
+        );
+      });
+  it('should let you log in', async () => {
+   let input =  page.$('input[name="log-user"]');
+    page.type(input,"Martin1234");
+    let pass = page.$('input[name="log-pass"]');
+    page.type(pass,"1234");
+    page.click('button#form_button');
+
+    const pageUrl = await page.goto('http://127.0.0.1:5500/source/home.html');
+    assert.equals(pageUrl,'http://127.0.0.1:5500/source/home.html');
+  });
+});
