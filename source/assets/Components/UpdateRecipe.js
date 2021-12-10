4f22d1f1ecdcc2c6642fa9d1d07b346f986e6a61
Esther <c3qiu@ucsd.edu>
2021-12-10 08:26:19 -0800

    fixed the navigation

--- source/assets/Components/UpdateRecipe.js
+++ source/assets/Components/UpdateRecipe.js
@@ -185,7 +185,7 @@ class UpdateRecipePage extends HTMLElement {
         <div class="part2">
           <!--Add Image-->
           <label for="img"><p><strong>Change Image</strong></p></label>
-          <input type="file" id="img" name="img" accept="image/*" required/>
+          <input type="file" id="img" name="img" accept="image/*"/>
           <p><img id="recipeImage"/></p>
           <br>
           <br>
@@ -605,22 +605,34 @@ class UpdateRecipePage extends HTMLElement {
 
       //Moves back to home page once POST is called
       function afterFetch() {
-        router.addPage(`recipe_${recipe['id']}`, function () {
+        // router.addPage(`recipe_${recipe['id']}`, function () {
+        //   document
+        //     .getElementById('#section--update-recipe')
+        //     .classList.remove('shown');
+        //   document.getElementById('#section--recipe').classList.add('shown');
+        //   // Fetch and populate recipe page and add to recipe section
+        //   const recipePage = document.createElement('recipe-page');
+        //   recipePage.data = data;
+        //   recipePage.classList.add('shown');
+        //   document.getElementById('#section--recipe').innerHTML = '';
+        //   document.getElementById('#section--recipe').appendChild(recipePage);
+        // });
+        // router.navigate(`recipe_${recipe['id']}`);
+        router.addPage('profile', function () {
+          document.getElementById('#section--home').classList.remove('shown');
+          document
+            .getElementById('#section--search-bar')
+            .classList.remove('shown');
           document
             .getElementById('#section--update-recipe')
             .classList.remove('shown');
-          document.getElementById('#section--recipe').classList.add('shown');
-          // Fetch and populate recipe page and add to recipe section
-          const recipePage = document.createElement('recipe-page');
-          recipePage.data = data;
-          recipePage.classList.add('shown');
-          document.getElementById('#section--recipe').innerHTML = '';
-          document.getElementById('#section--recipe').appendChild(recipePage);
+
+          document.getElementById('#section--profile').classList.add('shown');
         });
-        router.navigate(`recipe_${recipe['id']}`);
+        router.navigate('profile');
 
         // Tell the profile page to update with the updated recipe list
-        document.querySelector('#section--profile').firstChild.update = '';
+        // document.querySelector('#section--profile').firstChild.update = '';
       }
 
       //Sends data to database
